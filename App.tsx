78f12cebf66a4d4dd04e1c13f63b21dd0dbefb7d
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-02-27 16:56:49 +0700

    First init

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+} from 'react-native';
+
+
+function App(): React.JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+}
+export default App;
